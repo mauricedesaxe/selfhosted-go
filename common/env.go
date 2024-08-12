548433c44570238399e04e11502195f4e9517a66
lazaralex98 <alex2012ro@gmail.com>
2024-08-12 12:00:47 +0300

    improve env var system

--- common/env.go
+++ common/env.go
@@ -1,76 +1,111 @@
 package common
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"reflect"
 	"slices"
 	"strings"
-
-	"github.com/joho/godotenv"
 )
 
 // Env is a globally-accessible variable that holds the environment variables
 // for the application. It is initialized in the init function. Just import it in your
 // package and use it to access the environment variables.
-var Env = Environment{}
+var Env = EnvVars{}
 
 func init() {
-	Env.init()
+	initEnv()
 }
 
-type Environment struct {
-	// Use the `env` tag to specify the name of the environment variable.
-	// Use the `default` tag to specify a default value. If a variable is not found
-	// and a default value is not specified, the application will panic.
-
-	// Application settings
-	ENVIRONMENT string `env:"ENVIRONMENT" default:"production" type:"enum" options:"development,production,test"`
-	BASE_URL    string `env:"BASE_URL" default:"http://localhost:3000"`
-	PORT        string `env:"PORT" default:"3000"`
+type EnvVars struct {
+	ENVIRONMENT string `json:"ENVIRONMENT" type:"enum" options:"development,production,test"`
+	BASE_URL    string `json:"BASE_URL" default:"http://localhost:3000"`
+	PORT        string `json:"PORT" default:"3000"`
 
 	// * Add more environment variables here
 }
 
-func (e *Environment) init() {
-	isTest := strings.HasSuffix(os.Args[0], ".test") || strings.Contains(os.Args[0], "/_test/")
-	var dotenvPath string
-	if isTest {
-		log.Println("Loading environment variables from: .env.test")
-		dotenvPath = ".env.test"
-	} else {
-		log.Println("Loading environment variables from: .env")
-		dotenvPath = ".env"
-	}
+type Environment struct {
+	Environment string  `json:"environment" type:"enum" options:"development,production,test"`
+	Production  EnvVars `json:"production"`
+	Development EnvVars `json:"development"`
+	Test        EnvVars `json:"test"`
+}
 
-	err := godotenv.Load(dotenvPath)
+func initEnv() {
+	// get env.json file
+	envFile, err := os.Open("env.json")
 	if err != nil {
-		log.Printf("Error loading .env file: %v", err)
+		log.Fatalf("Error opening env.json: %v", err)
+	}
+	defer envFile.Close()
+
+	// read env.json file
+	envBytes, err := io.ReadAll(envFile)
+	if err != nil {
+		log.Fatalf("Error reading env.json: %v", err)
 	}
 
-	val := reflect.ValueOf(e).Elem()
+	// unmarshal env.json file into Environment struct
+	var envConfig Environment
+	err = json.Unmarshal(envBytes, &envConfig)
+	if err != nil {
+		log.Fatalf("Error unmarshalling env.json: %v", err)
+	}
+
+	// extract the correct environment variables
+	environmentType := envConfig.Environment
+	var envVars EnvVars
+	if environmentType == "production" {
+		envVars = envConfig.Production
+	} else if environmentType == "development" {
+		envVars = envConfig.Development
+	} else if environmentType == "test" {
+		envVars = envConfig.Test
+	} else {
+		log.Fatalf("Invalid environment type: %s", environmentType)
+	}
+
+	// validate the environment variables based on the struct tags
+	log.Println("===============VALIDATING ENVIRONMENT VARIABLES===============")
+	envVars, err = validateEnvVars(envVars)
+	if err != nil {
+		log.Fatalf("Error validating environment variables: %v", err)
+	}
+	log.Println("===============VALIDATED ENVIRONMENT VARIABLES===============")
+
+	// set the global Env variable
+	Env = envVars
+}
+
+func validateEnvVars(envVars EnvVars) (EnvVars, error) {
+	val := reflect.ValueOf(&envVars).Elem()
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Type().Field(i)
-		envVar, ok := field.Tag.Lookup("env")
+		varName, ok := field.Tag.Lookup("json")
 		if !ok {
 			continue
 		}
-		envValue := os.Getenv(envVar)
-		if envValue == "" {
-			envValue, ok = field.Tag.Lookup("default")
+		varVal := val.Field(i).String()
+		if varVal == "" {
+			varVal, ok = field.Tag.Lookup("default")
 			if !ok {
-				log.Panicf("Environment variable %s not found", envVar)
+				return EnvVars{}, fmt.Errorf("environment variable %s not found", varName)
 			}
-			log.Printf("Using default value for %s: %s", envVar, envValue)
 		}
 
 		if field.Tag.Get("type") == "enum" {
 			optionsString := field.Tag.Get("options")
 			options := strings.Split(optionsString, ",")
-			if !slices.Contains(options, envValue) {
-				log.Panicf("Invalid value for %s. Expected one of: '%s'; got '%s'", envVar, optionsString, envValue)
+			if !slices.Contains(options, varVal) {
+				return EnvVars{}, fmt.Errorf("invalid value for %s. Expected one of: '%s'; got '%s'", varName, optionsString, varVal)
 			}
 		}
-		val.Field(i).SetString(envValue)
+		log.Println(varName, varVal)
+		val.Field(i).SetString(varVal)
 	}
+	return envVars, nil
 }
